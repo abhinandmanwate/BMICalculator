a3e1b94d085cc0472ca59976a275b729235e2034
Abhinand Manwate <abhinandmanwate123@gmail.com>
2023-04-07 18:04:50 +0530

    Logic Added and Testing done App working!!

--- app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -16,6 +16,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        EditText edtWeight, edtHightFt,edtHightIn;
+        Button btnCalculate;
+        TextView txtResult;
+
+        edtWeight =findViewById(R.id.edtWeight);
+        edtHightFt = findViewById(R.id.edtHightFt);
+        edtHightIn = findViewById(R.id.edtHightIn);
+        btnCalculate = findViewById(R.id.btnCalculate);
+        txtResult = findViewById(R.id.txtResult);
+
+        btnCalculate.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                int wt = Integer.parseInt(edtWeight.getText().toString());
+                int ft = Integer.parseInt(edtHightFt.getText().toString());
+                int in = Integer.parseInt(edtHightIn.getText().toString());
+
+
+                int totalIn = ft*12 + in;
+
+                double totalCm = totalIn*2.54;
+
+                double totalM = totalCm/100;
+
+                double bmi = wt/(totalM*totalM);
+
+                if(bmi>25){
+
+                    txtResult.setText("You're overweight!");
+                }else if(bmi<18) {
+                    txtResult.setText("You're under weight!");
+                }
+                else{
+
+                    txtResult.setText("You're Healthy");
+                }
+
+
+            }
+        });
+
 
     }
 }
